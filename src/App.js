667a2aecca3016cf0cb86f3d7163b2979acba0c4
Rahageer Saadman Islam <rahageer.saadman.islam@g.bracu.ac.bd>
2024-06-05 00:40:53 +0600

    Personalized Chat System

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-// App.js
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '0a2c1c10-c583-44da-b510-ee01ec0c771b';
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="0a2c1c10-c583-44da-b510-ee01ec0c771b"
-      userName="Reacttest"
-      userSecret="121212"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
-}
+};
+
 
 export default App;
